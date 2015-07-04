ae519d7c3b8e6dc4c0b352a95c0005ed7e6df5cf
Oladayo Osikoya <alwaysdeone@gmail.com>
2015-07-04 01:10:56 +0100

    Set up urls and index view.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Radcheck
 
-from .forms import RadcheckAdminForm
-
 @admin.register(Radcheck)
 class RadcheckAdmin(admin.ModelAdmin):
     pass
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import createAccountForm
+
+def index(request):
+    if request.method == 'POST':
+        form = createAccountForm(request.POST)
+        if form.is_valid():
+            pass
+    else:
+        form = createAccountForm()
+  
+    context = {'form': form}
+    return render(request, 'accounts/index.html', context)
