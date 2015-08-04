a0353a7525847eb189b18302f0b6b91610b16c19
Oladayo Osikoya <alwaysdeone@gmail.com>
2015-08-04 22:10:55 +0100

    Add global directories for static files and templates.

--- spectrabilling/settings.py
+++ spectrabilling/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'spectrabilling.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,6 +105,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 LOGIN_REDIRECT_URL = '/accounts/dashboard/'
 
 from .user_settings import *
