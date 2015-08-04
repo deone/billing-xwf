b6a3e7b03123e5d9e64f965e8ea4175b0155c44c
Oladayo Osikoya <alwaysdeone@gmail.com>
2015-08-04 20:47:28 +0100

    Make Subscriber model display with User model in admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Radcheck
+from django.contrib.auth.admin import UserAdmin
+
+from .models import *
+
+class SubscriberInline(admin.StackedInline):
+    model = Subscriber
+    can_delete = False
+    verbose_name_plural = 'subscribers'
 
 @admin.register(Radcheck)
 class RadcheckAdmin(admin.ModelAdmin):
     pass
+
+class UserAdmin(UserAdmin):
+    inlines = (SubscriberInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
