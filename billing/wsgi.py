9e5c2fa73d4b0579c7f6a634634d6c7e77c40867
Oladayo Osikoya <dayo@incisia.com>
2015-08-14 15:38:13 +0100

    Change spectrabilling.settings to billing.settings.

--- billing/wsgi.py
+++ billing/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spectrabilling.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "billing.settings_live")
 
 application = get_wsgi_application()
