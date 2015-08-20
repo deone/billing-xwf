35412c902416fd491655349b6df5a7ce4260f00b
Oladayo Osikoya <dayo@incisia.com>
2015-08-20 12:57:00 +0100

    Test: - User can fetch index() and it uses the right template and form. - dashboard() redirects when users tries to access it with   authenticating.

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from .forms import CreateAccountForm
+
+class AccountsViewTests(TestCase):
+
+    def test_index_view(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Create Account')
+        self.assertTemplateUsed(response, 'accounts/index.html')
+        self.assertTrue(isinstance(response.context['form'], CreateAccountForm))
+
+    def test_dashboard_view_without_authentication(self):
+        response = self.client.get(reverse('accounts:dashboard'))
+        self.assertRedirects(response, ''.join([reverse('accounts:login'), '?next=/accounts/dashboard/']))
