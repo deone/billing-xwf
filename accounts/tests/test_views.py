ebec54eacededc501ef01b3e3ea7ea045d290044
Oladayo Osikoya <dayo@incisia.com>
2015-08-23 18:11:48 +0100

    Test login failure. Also test auth_and_login helper failure.

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -4,11 +4,13 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.sessions.middleware import SessionMiddleware
 
+from ..helpers import auth_and_login
 from ..forms import CreateAccountForm, LoginForm
 from ..views import index
 
 class AccountsViewTests(TestCase):
     def setUp(self):
+        self.c = Client()
         self.factory = RequestFactory()
         self.middleware = SessionMiddleware()
         self.user = User.objects.create_user('a@a.com', 'a@a.com', '12345')
@@ -39,16 +41,24 @@ class AccountsViewTests(TestCase):
 
         self.assertTrue(response.status_code, 302)
 
-    def test_dashboard_without_authentication(self):
+    def test_dashboard(self):
         response = self.client.get(reverse('accounts:dashboard'))
         self.assertRedirects(response, ''.join(
           [reverse(settings.LOGIN_URL), '?next=', reverse('accounts:dashboard')]
         ))
 
-    def test_login_dashboard_redirect(self):
-        c = Client()
-        response = c.post(reverse('accounts:login'), {'username': 'a@a.com', 'password': '12345'})
+    def test_login(self):
+        response = self.c.post(reverse('accounts:login'), {'username': 'a@a.com', 'password': '12345'})
         self.assertRedirects(response, reverse(settings.LOGIN_REDIRECT_URL))
+
+    def test_login_fail(self):
+        response = self.c.post(reverse('accounts:login'), {'username': 'b@b.com', 'password': '12345'})
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+    def test_auth_and_login_fail(self):
+        request = self.factory.get(reverse('accounts:login'))
+        response = auth_and_login(request, 'b@b.com', '1234')
+        self.assertEquals(response, False)
 
     def test_captive(self):
         get_params = "?login_url=https%3A%2F%2Fn110.network-auth.com%2Fsplash%2Flogin%3Fmauth%3DMMzZUJGqtrsmvkKw6ktCkcNsuBgluav4m2vgE4p-nFliz6lOzP99ntPzZAjvJ_Yit73ZfWwRDIzoEAwzZSuErRpQwdfD0vVA3XjsLLlK8UNiucySNAij7FEqEAF9osnXpWioNcUpyn7BYW8pP5C-wdZAQpLAWS-lv4UTivlfTUn92n4RxMaWG52Q%26continue_url%3Dhttps%253A%252F%252Fn110.network-auth.com%252Fsplash%252Fconnected%253Fhide_terms%253Dtrue&continue_url=https%3A%2F%2Fn110.network-auth.com%2Fsplash%2Fconnected%3Fhide_terms%3Dtrue&ap_mac=00%3A18%3A0a%3Af2%3Ade%3A20&ap_name=Djungle+HQ+02&ap_tags=office-accra+recently-added&client_mac=4c%3A8d%3A79%3Ad7%3A6b%3A28&client_ip=192.168.2.65"
