1682d80d2c72b3fc885d9a0d565047aaaaca0f9b
Oladayo Osikoya <dayo@incisia.com>
2015-08-23 17:27:56 +0100

    Add all radius models to the accounts app. This app manages all radius models.

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,98 @@ from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 
+class Nas(models.Model):
+    nasname = models.CharField(max_length=128)
+    shortname = models.CharField(max_length=32, blank=True, null=True)
+    type = models.CharField(max_length=30, blank=True, null=True)
+    ports = models.IntegerField(blank=True, null=True)
+    secret = models.CharField(max_length=60)
+    server = models.CharField(max_length=64, blank=True, null=True)
+    community = models.CharField(max_length=50, blank=True, null=True)
+    description = models.CharField(max_length=200, blank=True, null=True)
+
+    class Meta:
+        db_table = 'nas'
+
+
+class Radacct(models.Model):
+    radacctid = models.BigIntegerField(primary_key=True)
+    acctsessionid = models.CharField(max_length=64)
+    acctuniqueid = models.CharField(unique=True, max_length=32)
+    username = models.CharField(max_length=64)
+    groupname = models.CharField(max_length=64)
+    realm = models.CharField(max_length=64, blank=True, null=True)
+    nasipaddress = models.CharField(max_length=15)
+    nasportid = models.CharField(max_length=15, blank=True, null=True)
+    nasporttype = models.CharField(max_length=32, blank=True, null=True)
+    acctstarttime = models.DateTimeField(blank=True, null=True)
+    acctupdatetime = models.DateTimeField(blank=True, null=True)
+    acctstoptime = models.DateTimeField(blank=True, null=True)
+    acctinterval = models.IntegerField(blank=True, null=True)
+    acctsessiontime = models.IntegerField(blank=True, null=True)
+    acctauthentic = models.CharField(max_length=32, blank=True, null=True)
+    connectinfo_start = models.CharField(max_length=50, blank=True, null=True)
+    connectinfo_stop = models.CharField(max_length=50, blank=True, null=True)
+    acctinputoctets = models.BigIntegerField(blank=True, null=True)
+    acctoutputoctets = models.BigIntegerField(blank=True, null=True)
+    calledstationid = models.CharField(max_length=50)
+    callingstationid = models.CharField(max_length=50)
+    acctterminatecause = models.CharField(max_length=32)
+    servicetype = models.CharField(max_length=32, blank=True, null=True)
+    framedprotocol = models.CharField(max_length=32, blank=True, null=True)
+    framedipaddress = models.CharField(max_length=15)
+
+    class Meta:
+        db_table = 'radacct'
+
+class Radgroupcheck(models.Model):
+    groupname = models.CharField(max_length=64)
+    attribute = models.CharField(max_length=64)
+    op = models.CharField(max_length=2)
+    value = models.CharField(max_length=253)
+
+    class Meta:
+        db_table = 'radgroupcheck'
+
+
+class Radgroupreply(models.Model):
+    groupname = models.CharField(max_length=64)
+    attribute = models.CharField(max_length=64)
+    op = models.CharField(max_length=2)
+    value = models.CharField(max_length=253)
+
+    class Meta:
+        db_table = 'radgroupreply'
+
+
+class Radpostauth(models.Model):
+    username = models.CharField(max_length=64)
+    pass_field = models.CharField(db_column='pass', max_length=64)  # Field renamed because it was a Python reserved word.
+    reply = models.CharField(max_length=32)
+    authdate = models.DateTimeField()
+
+    class Meta:
+        db_table = 'radpostauth'
+
+
+class Radreply(models.Model):
+    username = models.CharField(max_length=64)
+    attribute = models.CharField(max_length=64)
+    op = models.CharField(max_length=2)
+    value = models.CharField(max_length=253)
+
+    class Meta:
+        db_table = 'radreply'
+
+
+class Radusergroup(models.Model):
+    username = models.CharField(max_length=64)
+    groupname = models.CharField(max_length=64)
+    priority = models.IntegerField()
+
+    class Meta:
+        db_table = 'radusergroup'
+
 class Radcheck(models.Model):
     username = models.CharField(max_length=64)
     attribute = models.CharField(max_length=64)
@@ -13,7 +105,6 @@ class Radcheck(models.Model):
     class Meta:
         # For tests involving models with managed=False, it’s up to you to
         # ensure the correct tables are created as part of the test setup.
-        managed = False
         db_table = 'radcheck'
 
     def __str__(self):
