578998433f6a634f47a8bda4ba8758a8d720a31b
Oladayo Osikoya <dayo@incisia.com>
2015-08-23 18:43:17 +0100

    Test Radcheck.__str__()

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from ..models import Subscriber
+from ..models import Subscriber, Radcheck
+from ..helpers import md5_password
 
 class AccountsModelsTests(TestCase):
 
@@ -14,3 +15,9 @@ class AccountsModelsTests(TestCase):
         phone_number = country_code + '8029299274'
         subscriber = Subscriber.objects.create(user=self.user, country=self.country, phone_number=phone_number)
         self.assertEquals(subscriber.__str__(), 'a@a.com')
+
+    def test_radcheck(self):
+        entry = Radcheck.objects.create(
+            username=self.user.username, attribute='MD5-Password', op=':=', value=md5_password('12345')
+        )
+        self.assertEquals(entry.__str__(), 'a@a.com')
