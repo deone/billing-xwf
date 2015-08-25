f9342e6e9daa714044c26e45cf7200bf67485222
Oladayo Osikoya <dayo@incisia.com>
2015-08-25 11:23:32 +0100

    Add email verification fields to Subscriber.

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@
 from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Nas(models.Model):
     nasname = models.CharField(max_length=128)
@@ -144,6 +145,8 @@ class Subscriber(models.Model):
     country = models.CharField(max_length=3, choices=COUNTRY_CHOICES, default=GHANA)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=15) # validators should be a list
+    email_verified = models.BooleanField(default=False)
+    date_verified = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.user.username
