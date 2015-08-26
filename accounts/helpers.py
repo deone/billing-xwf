7c252d6865c6c991059724a62a0cccace8c504ee
Oladayo Osikoya <dayo@incisia.com>
2015-08-26 12:26:34 +0100

    Move verfication email sending into helpers.

--- accounts/helpers.py
+++ accounts/helpers.py
@@ -1,4 +1,11 @@
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.conf import settings
 
 import hashlib
 
@@ -16,3 +23,14 @@ def md5_password(password):
     m.update(password)
 
     return m.hexdigest()
+
+def send_verification_mail(request, user):
+    current_site = get_current_site(request)
+    site_name = current_site.name
+    domain = current_site.domain
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    token = default_token_generator.make_token(user)
+    verification_link = "http://%s%s" % (domain,
+        reverse('accounts:verify_email', kwargs={'uidb64': uid, 'token': token}))
+    send_mail('Verify Email', verification_link,
+        settings.DEFAULT_FROM_EMAIL, [user.email], fail_silently=False)
