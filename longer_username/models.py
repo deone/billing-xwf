44c7f87e90dff889571ecbc9ac16aea75a386850
Dayo Osikoya <alwaysdeone@gmail.com>
2015-08-29 16:40:55 +0100

    Add new app 'longer_username' to change User max_length from 30 to 100.

---
+++ longer_username/models.py
@@ -0,0 +1,7 @@
+from django.db.models.signals import class_prepared
+
+def longer_username(sender, *args, **kwargs):
+    if sender.__name__ == "User" and sender.__module__ == "django.contrib.auth.models":
+        sender._meta.get_field("username").max_length = 100
+
+class_prepared.connect(longer_username)
