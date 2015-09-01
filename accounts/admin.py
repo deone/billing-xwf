bce5bec9cdfe0190b5f956252b0a50dd36765762
Dayo Osikoya <alwaysdeone@gmail.com>
2015-09-01 10:20:33 +0100

    Sub-class UserCreationForm and UserChangeForm and set help_text.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
 
 from .models import *
+
+help_text = """Required. 100 characters or fewer. Letters, digits and @/./+/-/_ only."""
 
 class SubscriberInline(admin.StackedInline):
     model = Subscriber
     can_delete = False
     verbose_name_plural = 'subscribers'
 
-class UserAdmin(UserAdmin):
+class AccountsUserCreationForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super(AccountsUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].help_text = help_text
+
+class AccountsUserChangeForm(UserChangeForm):
+
+    def __init__(self, *args, **kwargs):
+        super(AccountsUserChangeForm, self).__init__(*args, **kwargs)
+        self.fields['username'].help_text = help_text
+
+class AccountsUserAdmin(UserAdmin):
+    form = AccountsUserChangeForm
+    add_form = AccountsUserCreationForm
     inlines = (SubscriberInline, )
 
 admin.site.unregister(User)
-# admin.site.register(User, UserAdmin)
+admin.site.register(User, AccountsUserAdmin)
