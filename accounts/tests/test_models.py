afe11a1212d130f0e76bd1d48439e263c99d4175
Dayo Osikoya <alwaysdeone@gmail.com>
2015-09-03 01:00:29 +0100

    Test GroupAccount and AccessPoint models.

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from ..models import Subscriber, Radcheck
+from ..models import Subscriber, Radcheck, GroupAccount, AccessPoint
 from ..helpers import md5_password
 
 class AccountsModelsTests(TestCase):
@@ -21,3 +21,45 @@ class AccountsModelsTests(TestCase):
             username=self.user.username, attribute='MD5-Password', op=':=', value=md5_password('12345')
         )
         self.assertEquals(entry.__str__(), 'a@a.com')
+
+class GroupAccountTests(TestCase):
+
+    def setUp(self):
+        self.group = GroupAccount.objects.create(name='CUG', max_user_quantity=10)
+
+    def test__str__(self):
+        self.assertEquals(self.group.__str__(), 'CUG')
+
+class AccessPointTests(TestCase):
+
+    def setUp(self):
+        self.group1 = GroupAccount.objects.create(name='CUG', max_user_quantity=10)
+        self.group2 = GroupAccount.objects.create(name='LUG', max_user_quantity=10)
+        self.ap = AccessPoint.objects.create(name='Djungle HQ 02', mac_address='00:18:0A:F2:DE:20')
+        self.user = User.objects.create_user('a@a.com', 'a@a.com', '12345')
+
+        Subscriber.objects.create(user=self.user, country='GHA', phone_number='+233542751610')
+
+    def test__str__(self):
+        self.assertEquals(self.ap.__str__(), 'Djungle HQ 02')
+
+    def test_allows_ap_private(self):
+        self.assertFalse(self.ap.allows(self.user))
+
+    def test_allows_ap_public(self):
+        self.ap.status = 'PUB'
+        self.ap.save()
+        self.assertTrue(self.ap.allows(self.user))
+
+    def test_allows_ap_subscriber_same_group(self):
+        self.ap.group = self.user.subscriber.group = self.group1
+        self.ap.save()
+        self.user.subscriber.save()
+        self.assertTrue(self.ap.allows(self.user))
+
+    def test_allows_ap_subscriber_different_group(self):
+        self.ap.group = self.group1
+        self.user.subscriber.group = self.group2
+        self.ap.save()
+        self.user.subscriber.save()
+        self.assertFalse(self.ap.allows(self.user))
