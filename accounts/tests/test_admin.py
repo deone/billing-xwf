75cd27eb6f201e98cf1ea90fba5f9dc4720b6b17
Dayo Osikoya <alwaysdeone@gmail.com>
2015-09-09 00:22:53 +0100

    Test SubscriberAdminForm.

--- accounts/tests/test_admin.py
+++ accounts/tests/test_admin.py
@@ -1,13 +1,15 @@
 from django import forms
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 from ..models import GroupAccount
-from ..admin import AccountsUserCreationForm, AccountsUserChangeForm, AccessPointAdminForm
+from ..admin import AccountsUserCreationForm, AccountsUserChangeForm, AccessPointAdminForm, SubscriberAdminForm
 
 class AdminFormsTest(TestCase):
 
     def setUp(self):
         self.help_text = "Required. 100 characters or fewer. Letters, digits and @/./+/-/_ only."
+        self.user = User.objects.create(username='b@b.com', password='12345')
         self.ga = GroupAccount.objects.create(name='CUG', max_user_quantity=10)
     
     def test_accounts_user_creation_form(self):
@@ -29,3 +31,16 @@ class AdminFormsTest(TestCase):
         form = AccessPointAdminForm({'name': 'HQ', 'group': self.ga.pk, 'mac_address': '00:18:0A:F2:DE:20', 'status': 'PRV'})
         self.assertTrue(form.is_valid())
         # print form.clean()
+
+    def test_subscriber_admin_form(self):
+        form = SubscriberAdminForm({
+          'phone_number': '0542751610',
+          'group': self.ga.pk,
+          'country': 'GHA',
+          'date_verified': None,
+          'user': self.user.pk,
+          'email_verified': False,
+          'id': None})
+        subscriber = form.save()
+        self.assertEqual(subscriber.phone_number, '+233542751610')
+        self.assertEqual(subscriber.user.email, 'b@b.com')
