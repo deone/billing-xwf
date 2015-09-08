78ad41eff77004a345828a739c26d42cef0f9043
Dayo Osikoya <alwaysdeone@gmail.com>
2015-09-09 00:25:55 +0100

    Create app packages. Define SPEED_CHOICES and VOLUME_CHOICES.

--- billing/user_settings.py
+++ billing/user_settings.py
@@ -1,1 +1,18 @@
-SERV_PKG = False
+SPEED_CHOICES = (
+    ('1', '1Mbps'),
+    ('1.5', '1.5Mbps'),
+    ('2', '2Mbps'),
+)
+
+VOLUME_CHOICES = (
+    ('1', '1GB'),
+    ('3', '3GB'),
+    ('5', '5GB'),
+    ('8', '8GB'),
+    ('10', '10GB'),
+    ('12', '12GB'),
+    ('15', '15GB'),
+    ('20', '20GB'),
+    ('25', '25GB'),
+    ('Unlimited', 'Unlimited'),
+)
