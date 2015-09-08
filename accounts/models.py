2c876d6cd88d347ab19bb5203dd54864fddb62f7
Dayo Osikoya <alwaysdeone@gmail.com>
2015-09-08 14:45:28 +0100

    Make Subscriber.date_verified blank=True.

--- accounts/models.py
+++ accounts/models.py
@@ -155,7 +155,7 @@ class Subscriber(models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=15) # validators should be a list
     email_verified = models.BooleanField(default=False)
-    date_verified = models.DateTimeField(null=True)
+    date_verified = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.user.username
