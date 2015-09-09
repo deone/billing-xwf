5d228d320bdab59513dad5b04b7f9c2d3ce95c02
Dayo Osikoya <alwaysdeone@gmail.com>
2015-09-09 01:48:48 +0100

    Add __str__() to Package.

--- packages/models.py
+++ packages/models.py
@@ -5,3 +5,6 @@ class Package(models.Model):
     package_type = models.CharField(max_length=7, choices=settings.PACKAGE_TYPES)
     volume = models.CharField(max_length=9, choices=settings.VOLUME_CHOICES)
     speed = models.CharField(max_length=3, choices=settings.SPEED_CHOICES, default='1.5')
+
+    def __str__(self):
+        return "%s %sGB" % (self.package_type, self.volume)
