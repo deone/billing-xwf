d2207381f0b440e78573860a5118c603930995a4
Dayo Osikoya <alwaysdeone@gmail.com>
2015-09-10 22:28:08 +0100

    Set Meta options and help_text on GroupPackageSubscription model and stop field. Set stop field in GroupPackageSubscriptionAdminForm.save().

--- packages/admin.py
+++ packages/admin.py
@@ -1,5 +1,26 @@
+from django import forms
 from django.contrib import admin
 
-from .models import Package
+from .models import *
+
+from datetime import timedelta
+
+class GroupPackageSubscriptionAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = GroupPackageSubscription
+        exclude = ()
+
+    def save(self, commit=True):
+        grp_pkg_sub = super(GroupPackageSubscriptionAdminForm, self).save(commit=False)
+        package_period = timedelta(hours=settings.PACKAGE_TYPES_HOURS_MAP[grp_pkg_sub.package.package_type])
+        grp_pkg_sub.stop = grp_pkg_sub.start + package_period
+        grp_pkg_sub.save()
+
+        return grp_pkg_sub
+
+class GroupPackageSubscriptionAdmin(admin.ModelAdmin):
+    form = GroupPackageSubscriptionAdminForm
 
 admin.site.register(Package)
+admin.site.register(GroupPackageSubscription, GroupPackageSubscriptionAdmin)
--- packages/models.py
+++ packages/models.py
@@ -18,7 +18,11 @@ class GroupPackageSubscription(models.Model):
     group = models.ForeignKey(GroupAccount)
     package = models.ForeignKey(Package)
     start = models.DateTimeField()
-    stop = models.DateTimeField(blank=True)
+    stop = models.DateTimeField(blank=True, help_text="The time this subscription expires. You are not allowed to set this.")
+
+    class Meta:
+        verbose_name = "Group Package Subscription"
+        ordering = ['-stop']
 
     def __str__(self):
         return "%s %s %s" % (self.group.name, self.package.package_type, self.stop)
