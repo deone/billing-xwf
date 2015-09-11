00a585d64e3ee1627169546aafbc117e1ed0aa4b
Dayo Osikoya <alwaysdeone@gmail.com>
2015-09-11 18:34:35 +0100

    Test GroupPackageSubscriptionAdminForm.save().

---
+++ packages/tests/test_admin.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.utils import timezone
+from django.conf import settings
+
+from datetime import timedelta
+
+from ..admin import GroupPackageSubscriptionAdminForm
+from ..models import Package
+from accounts.models import GroupAccount
+
+class AdminFormsTest(TestCase):
+    
+    def setUp(self):
+        self.package = Package.objects.create(package_type='Daily', volume='3', speed='1.5')
+        self.ga = GroupAccount.objects.create(name='CUG', max_no_of_users=10)
+    
+    def test_GroupPackageSubscriptionAdminForm_save(self):
+        now = timezone.now()
+        form = GroupPackageSubscriptionAdminForm({
+            'stop': None,
+            'start': now,
+            'group': self.ga.pk,
+            'package': self.package.pk
+        })
+        gps = form.save() 
+        self.assertEqual(gps.stop, gps.start + timedelta(hours=settings.PACKAGE_TYPES_HOURS_MAP[self.package.package_type]))
