fae6a3d41bea3ac39dfc03417762fbf859d9e4af
Dayo Osikoya <alwaysdeone@gmail.com>
2015-09-22 23:24:59 +0100

    Remove former email lists. Add id_generator and group account to bulk user creation script.

--- create_users.py
+++ create_users.py
@@ -11,20 +11,30 @@ from django.contrib.auth.models import User
 from accounts.models import Subscriber, Radcheck
 from accounts.helpers import md5_password
 
-_file = "emails.csv"
+_file = "test.csv"
 
 # We can use python to process the raw data file.
 
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 with open(_file, 'r') as f:
-    for line in f:
-        first_name, last_name, email = line.split(',')
-        email = email[:-1]
-        password = '12345'
-        user = User.objects.create_user(email, email, password)
-        user.first_name = first_name
-        user.last_name = last_name
-        user.save()
+    lines = f.readlines()
 
-        Subscriber.objects.create(is_group_admin=False, country='GHA', email_verified=False, user=user)
+for line in lines:
+    first_name, last_name, email = line.split(',')
+    email = email[:-1]
+    password = id_generator()
+    user = User.objects.create_user(email, email, password)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.save()
 
-        Radcheck.objects.create(username=email, attribute='MD5-Password', op=':=', value=md5_password(password))
+    group = GroupAccount.objects.get(name__exact="Koforidua Polytechnic")
+
+    Subscriber.objects.create(group=group, is_group_admin=False, country='GHA', email_verified=False, user=user)
+
+    Radcheck.objects.create(user=user, username=email, attribute='MD5-Password', op=':=', value=md5_password(password))
+
+    with open('done.csv', 'a') as f:
+        f.write(email + ',' + password)
