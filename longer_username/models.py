8ce6b42ed0d0958a65e17ad032ce08aa10660c01
Dayo Osikoya <alwaysdeone@gmail.com>
2015-09-29 15:27:08 +0100

    Change User model username field MaxLengthValidator to one that takes 100 as argument.

--- longer_username/models.py
+++ longer_username/models.py
@@ -1,7 +1,12 @@
 from django.db.models.signals import class_prepared
+from django.core.validators import MaxLengthValidator
 
 def longer_username(sender, *args, **kwargs):
     if sender.__name__ == "User" and sender.__module__ == "django.contrib.auth.models":
-        sender._meta.get_field("username").max_length = 100
+        max_length = 100
+        field = sender._meta.get_field("username")
+        field.max_length = max_length
+        field.validators.pop()
+        field.validators.append(MaxLengthValidator(int(max_length)))
 
 class_prepared.connect(longer_username)
