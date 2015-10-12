a603bdef2f5dfba412efb172ca0b07ce063c16e3
Dayo Osikoya <alwaysdeone@gmail.com>
2015-10-12 22:55:51 +0100

    Use Client() instead of TestCase.client.

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -8,7 +8,7 @@ from django.contrib.messages import get_messages
 from django.utils import timezone
 
 from ..helpers import auth_and_login, make_context
-from ..forms import CreateUserForm, LoginForm, BulkUserUploadForm
+from ..forms import CreateUserForm, LoginForm, BulkUserUploadForm, EditUserForm
 from ..views import index, resend_mail, add_user, buy_package, upload_user_list, edit_user
 from ..models import Subscriber, GroupAccount
 
@@ -29,7 +29,7 @@ class AccountsViewsTests(TestCase):
             volume='3', speed='1.5')
 
     def test_index_get(self):
-        response = self.client.get(reverse('index'))
+        response = self.c.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Create Account')
         self.assertTemplateUsed(response, 'accounts/index.html')
@@ -56,7 +56,7 @@ class AccountsViewsTests(TestCase):
         self.assertEqual(response.get('location'), reverse('accounts:dashboard'))
 
     def test_dashboard_login_redirect(self):
-        response = self.client.get(reverse('accounts:dashboard'))
+        response = self.c.get(reverse('accounts:dashboard'))
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, ''.join(
           [reverse(settings.LOGIN_URL), '?next=', reverse('accounts:dashboard')]
@@ -111,7 +111,7 @@ class AccountsViewsTests(TestCase):
 
     def test_captive(self):
         get_params = "?login_url=https%3A%2F%2Fn110.network-auth.com%2Fsplash%2Flogin%3Fmauth%3DMMzZUJGqtrsmvkKw6ktCkcNsuBgluav4m2vgE4p-nFliz6lOzP99ntPzZAjvJ_Yit73ZfWwRDIzoEAwzZSuErRpQwdfD0vVA3XjsLLlK8UNiucySNAij7FEqEAF9osnXpWioNcUpyn7BYW8pP5C-wdZAQpLAWS-lv4UTivlfTUn92n4RxMaWG52Q%26continue_url%3Dhttps%253A%252F%252Fn110.network-auth.com%252Fsplash%252Fconnected%253Fhide_terms%253Dtrue&continue_url=https%3A%2F%2Fn110.network-auth.com%2Fsplash%2Fconnected%3Fhide_terms%3Dtrue&ap_mac=00%3A18%3A0a%3Af2%3Ade%3A20&ap_name=Djungle+HQ+02&ap_tags=office-accra+recently-added&client_mac=4c%3A8d%3A79%3Ad7%3A6b%3A28&client_ip=192.168.2.65"
-        response = self.client.get(''.join([reverse('captive'), get_params]))
+        response = self.c.get(''.join([reverse('captive'), get_params]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Log In')
         self.assertTrue(isinstance(response.context['form'], LoginForm))
@@ -120,22 +120,22 @@ class AccountsViewsTests(TestCase):
 
     def test_captive_with_error_message(self):
         get_params = "?login_url=https%3A%2F%2Fn110.network-auth.com%2Fsplash%2Flogin%3Fmauth%3DMMzZUJGqtrsmvkKw6ktCkcNsuBgluav4m2vgE4p-nFliz6lOzP99ntPzZAjvJ_Yit73ZfWwRDIzoEAwzZSuErRpQwdfD0vVA3XjsLLlK8UNiucySNAij7FEqEAF9osnXpWioNcUpyn7BYW8pP5C-wdZAQpLAWS-lv4UTivlfTUn92n4RxMaWG52Q%26continue_url%3Dhttps%253A%252F%252Fn110.network-auth.com%252Fsplash%252Fconnected%253Fhide_terms%253Dtrue&error_message=Access+denied+for+herbertellisspectradjungle%40spectrawireless.com&continue_url=https%3A%2F%2Fn110.network-auth.com%2Fsplash%2Fconnected%3Fhide_terms%3Dtrue&ap_mac=00%3A18%3A0a%3Af2%3Ade%3A20&ap_name=Djungle+HQ+02&ap_tags=office-accra+recently-added&client_mac=4c%3A8d%3A79%3Ad7%3A6b%3A28&client_ip=192.168.2.65"
-        response = self.client.get(''.join([reverse('captive'), get_params]))
+        response = self.c.get(''.join([reverse('captive'), get_params]))
         self.assertEqual(response.status_code, 200)
         self.assertTrue('error_message' in response.context)
 
     def test_captive_without_get_params(self):
-        response = self.client.get(reverse('captive'))
+        response = self.c.get(reverse('captive'))
         self.assertEqual(response.status_code, 404)
 
     def test_success(self):
         get_params = "?logout_url=https%3A%2F%2Fn110.network-auth.com%2Fsplash%2Flogout%3Fkey%3DMM7n9oxmBMVzgXgqkvAbLsLTh2cP7lcZdnhrqPRdHlIqzFHCNSRkxoiKzMGmTDQw7dGd092BdPfUs"
-        response = self.client.get(''.join([reverse('success'), get_params]))
+        response = self.c.get(''.join([reverse('success'), get_params]))
         self.assertEqual(response.status_code, 200)
         self.assertTrue('logout_url' in response.context)
 
     def test_success_without_get_params(self):
-        response = self.client.get(reverse('success'))
+        response = self.c.get(reverse('success'))
         self.assertEqual(response.status_code, 200)
 
     def test_verify_email(self):
@@ -146,7 +146,7 @@ class AccountsViewsTests(TestCase):
         request = self.factory.get(reverse('index'))
         context = make_context(self.user)
 
-        response = self.client.get(reverse('accounts:verify_email',
+        response = self.c.get(reverse('accounts:verify_email',
           kwargs={'uidb64':context['uid'], 'token': context['token']}))
 
         subscriber = Subscriber.objects.get(pk=self.subscriber.pk)
@@ -158,7 +158,7 @@ class AccountsViewsTests(TestCase):
         self.assertNotEqual(subscriber.date_verified, None)
 
     def test_verify_email_404(self):
-        response = self.client.get(reverse('accounts:verify_email',
+        response = self.c.get(reverse('accounts:verify_email',
           kwargs={'uidb64':'Ng', 'token': '44l-013ea9fff05d175d1ccb'}))
         self.assertEqual(response.status_code, 404)
 
@@ -310,16 +310,21 @@ class AccountsViewsTests(TestCase):
         response = self.c.get(reverse('accounts:edit_user', kwargs={'pk':user.pk}))
 
         self.assertEqual(response.status_code, 200)
+        self.assertTrue('form' in response.context)
+        self.assertTrue(isinstance(response.context['form'], EditUserForm))
 
     """ def test_edit_user_post(self):
-        # self.c.post(reverse('accounts:login'), {'username': 'a@a.com', 'password': '12345'})
-        user = User.objects.create(email='c@c.com', username='c@c.com', password='12345', is_active=False)
+        user = User.objects.create(email='d@d.com', username='d@d.com', password='12345', is_active=False)
         subscriber = Subscriber.objects.create(user=user,
             country='GHA', phone_number=Subscriber.COUNTRY_CODES_MAP['GHA'] + '542751610',
             email_verified=True, date_verified=timezone.now())
 
-        # print 'Test', user.pk
-        # response = self.c.get(reverse('accounts:edit_user', kwargs={'pk':user.pk}))
+        response = self.c.post(reverse('accounts:edit_user', kwargs={'pk':user.pk}), data={
+              'username': 'b@de.com',
+              'first_name': 'Ola',
+              'last_name': 'Ade',
+              'phone_number': '0542751233'
+              })
 
         request = self.factory.post(reverse('accounts:edit_user', kwargs={'pk':user.pk}),
             data={
@@ -328,7 +333,8 @@ class AccountsViewsTests(TestCase):
               'last_name': 'Ade',
               'phone_number': '0542751233'
               })
-        print request
+
+        request = self.factory.post(reverse('accounts:edit_user', kwargs={'pk':user.pk}))
         request.user = self.user
 
         self.session.process_request(request)
