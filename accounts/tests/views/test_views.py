cabc790187c7965c0eb5eb109d3c8463eea020ca
Dayo Osikoya <alwaysdeone@gmail.com>
2015-10-28 16:07:46 +0100

    Move add_user test into separate file.

--- accounts/tests/views/test_views.py
+++ accounts/tests/views/test_views.py
@@ -42,43 +42,7 @@ class AccountsViewsTests(TestCase):
 
     
 
-    def test_add_user_get(self):
-        self.c.post(reverse('accounts:login'), {'username': 'a@a.com', 'password': '12345'})
-        response = self.c.get(reverse('accounts:add_user'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('form' in response.context)
-        self.assertTrue(isinstance(response.context['form'], CreateUserForm))
-        self.assertTemplateUsed(response, 'accounts/add_user.html')
-
-    def test_add_user_post(self):
-        request = self.factory.post(reverse('accounts:add_user'),
-            data={
-              'username': 'b@b.com',
-              'password': '12345',
-              'first_name': 'Ola',
-              'last_name': 'Ade',
-              'confirm_password': '12345',
-              'country': 'GHA',
-              'phone_number': '0542751610'
-              })
-        request.user = self.user
-
-        self.session.process_request(request)
-        request.session.save()
-
-        messages = FallbackStorage(request)
-        setattr(request, '_messages', messages)
-
-        response = add_user(request)
-        storage = get_messages(request)
-
-        lst = []
-        for message in storage:
-            lst.append(message)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual('User added successfully.', lst[0].__str__())
-        self.assertEqual(response.get('location'), reverse('accounts:add_user'))
+    
 
     def test_buy_package_get(self):
         self.c.post(reverse('accounts:login'), {'username': 'a@a.com', 'password': '12345'})
