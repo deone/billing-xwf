348edec27c1b83001fd8e1bb5991153245d909d9
Dayo Osikoya <alwaysdeone@gmail.com>
2015-10-29 11:40:05 +0100

    Test get_balance.

--- accounts/tests/test_helpers.py
+++ accounts/tests/test_helpers.py
@@ -1,8 +1,9 @@
 from django.core.urlresolvers import reverse
 
-from ..helpers import auth_and_login
+from ..helpers import (auth_and_login, get_balance)
 
 from views import ViewsTests
+from ..models import RechargeAndUsage
 
 class HelpersTests(ViewsTests):
 
@@ -10,3 +11,23 @@ class HelpersTests(ViewsTests):
         request = self.factory.get(reverse('accounts:login'))
         response = auth_and_login(request, 'b@b.com', '1234')
         self.assertEqual(response, False)
+
+    def test_get_balance_zero(self):
+        # User has no recharge entry. Balance should be zero
+        self.assertEqual(get_balance(self.user), 0)
+
+    def test_get_balance_non_zero(self):
+        balance = get_balance(self.user)
+        card_value = 4
+        balance = balance + card_value
+
+        # Recharge user account
+        recharge = RechargeAndUsage.objects.create(
+            subscriber=self.subscriber,
+            amount=card_value,
+            balance=balance,
+            action='REC',
+            activity_id=30
+        )
+
+        self.assertEqual(get_balance(self.user), 4)
