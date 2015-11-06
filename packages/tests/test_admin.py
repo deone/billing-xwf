2a0843bf90ebfea792634218baccbce3ca8a18d2
Dayo Osikoya <alwaysdeone@gmail.com>
2015-11-06 04:42:09 +0100

    Test subscription_group, subscription_subscriber and subscription_package admin functions.

--- packages/tests/test_admin.py
+++ packages/tests/test_admin.py
@@ -5,8 +5,10 @@ from django.contrib.auth.models import User
 
 from datetime import timedelta
 
-from ..admin import PackageSubscriptionAdminForm, GroupPackageSubscriptionAdminForm
-from ..models import Package
+from ..admin import (
+    PackageSubscriptionAdminForm, GroupPackageSubscriptionAdminForm, subscription_package, subscription_group, subscription_subscriber
+)
+from ..models import *
 from accounts.models import GroupAccount, Subscriber
 
 class AdminFormsTest(TestCase):
@@ -40,3 +42,21 @@ class AdminFormsTest(TestCase):
         ps = form.save()
         self.assertEqual((ps.stop - ps.start).days,
             settings.PACKAGE_TYPES_HOURS_MAP[self.package.package_type] / 24)
+
+    def test_subscription_group(self):
+        ga = GroupAccount.objects.create(name='LFG', max_no_of_users=5)
+        gps = GroupPackageSubscription.objects.create(group=ga,
+            package=self.package, start=self.now,
+            stop=self.now + timedelta(hours=settings.PACKAGE_TYPES_HOURS_MAP[self.package.package_type]))
+
+        self.assertEqual(subscription_group(gps), 'LFG')
+
+    def test_subscription_subscriber_subscription_package(self):
+        user = User.objects.create_user('a@a.com', 'a@a.com', '12345')
+        subscriber = Subscriber.objects.create(user=user, country='GHA', phone_number='+233542751610')
+        ps = PackageSubscription.objects.create(
+            subscriber=subscriber, package=self.package, start=self.now,
+            stop=self.now + timedelta(hours=settings.PACKAGE_TYPES_HOURS_MAP[self.package.package_type]))
+
+        self.assertEqual(subscription_subscriber(ps), 'a@a.com')
+        self.assertEqual(subscription_package(ps), '1.5Mbps Delux Monthly 3GB')
