b2215bf58c04405864f3fc84b37980dfe5e41edf
Dayo Osikoya <alwaysdeone@gmail.com>
2015-12-21 14:46:36 +0100

    Create Radcheck and InstantVoucher entries when an API request is received.

--- packages/views.py
+++ packages/views.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 
-from .models import Package
+from accounts.models import Radcheck
+from accounts.helpers import md5_password
+from .models import Package, InstantVoucher
 
 @ensure_csrf_cookie
 def packages(request):
@@ -13,3 +15,22 @@ def packages(request):
         packages.append(tup)
     response.update({'code': 200, 'results': list(packages)})
     return JsonResponse(response)
+
+@ensure_csrf_cookie
+def insert_vouchers(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        package_id = request.POST['package_id']
+
+        radcheck = Radcheck.objects.create(username=username,
+                                    attribute='MD5-Password',
+                                    op=':=',
+                                    value=md5_password(password))
+        package = Package.objects.get(pk=package_id)
+
+        InstantVoucher.objects.create(radcheck=radcheck, package=package)
+    else:
+        pass
+
+    return JsonResponse({'status': 'ok'})
