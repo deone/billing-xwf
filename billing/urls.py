8213f39077076f38a66bb3d1e9234551dd487699
Dayo Osikoya <alwaysdeone@gmail.com>
2016-01-20 00:38:39 +0100

    Search for users by username, first_name and last_name.

--- billing/urls.py
+++ billing/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^captive/$', views.captive, name='captive'),
     url(r'^success/$', views.success, name='success'),
+    url(r'^search/$', views.search, name='search'),
     url(r'^resend_mail/$', views.resend_mail, name='resend_mail'),
     url(r'^accounts/', include('accounts.urls', namespace="accounts")),
     url(r'^packages/', include('packages.urls', namespace="packages")),
