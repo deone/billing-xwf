35723eec22b56a95eea486df95ac39f7a4dd7aaa
Dayo Osikoya <alwaysdeone@gmail.com>
2016-02-20 07:39:59 +0100

    Test toggle_status() without page argument.

--- accounts/tests/views/test_view_users.py
+++ accounts/tests/views/test_view_users.py
@@ -6,12 +6,20 @@ from ...views import view_users
 
 class ViewUsersTests(GroupAdminViewsTests):
 
-    def test_view_users_get(self):
+    def test_view_users(self):
         request = self.factory.post(reverse('accounts:users'))
         request.user = self.user
-
         self.session.process_request(request)
         request.session.save()
 
         response = view_users(request, page=2)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_users_page_None(self):
+        request = self.factory.post(reverse('accounts:users'), {'page': 1})
+        request.user = self.user
+        self.session.process_request(request)
+        request.session.save()
+
+        response = view_users(request)
         self.assertEqual(response.status_code, 200)
