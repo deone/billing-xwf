1993651ee476f9dba197e51fd8d401a5e5e2b25c
Dayo Osikoya <alwaysdeone@gmail.com>
2016-02-26 03:21:24 +0100

    Update settings.

--- billing/settings_production.py
+++ billing/settings_production.py
@@ -1,5 +1,24 @@
-from settings_test import *
+from settings import *
 
 DEBUG = False
 
 IP = '154.117.8.18'
+
+ALLOWED_HOSTS = [IP]
+
+SUCCESS_URL = "http://" + IP + ":8080/success/"
+
+DEFAULT_FROM_EMAIL = 'info@spectrawireless.com'
+
+# VMS
+VMS_URL = "http://" + IP + ":8090/vouchers/"
+
+VOUCHER_STUB_INSERT_URL = VMS_URL + "insert/"
+
+VOUCHER_STUB_DELETE_URL = VMS_URL + "delete/"
+
+VOUCHER_REDEEM_URL = VMS_URL + "redeem/"
+
+VOUCHER_INVALIDATE_URL = VMS_URL + "invalidate/"
+
+VOUCHER_SELL_URL = VMS_URL + "sell/"
