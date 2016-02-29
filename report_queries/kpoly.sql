d69aa8a830c3738c128f6aafb7a7a1c4232cee92
Dayo Osikoya <alwaysdeone@gmail.com>
2016-02-29 19:52:34 +0100

    Output it into csv file.

--- report_queries/kpoly.sql
+++ report_queries/kpoly.sql
@@ -1,3 +1,7 @@
 /* Return first_name, last_name and username for users in a given group */
 
-select a.first_name, a.last_name, a.username from auth_user as a join accounts_subscriber as b where a.id=b.user_id and b.group_id=2;
+select a.first_name, a.last_name, a.username from auth_user as a join accounts_subscriber as b where a.id=b.user_id and b.group_id=2
+INTO OUTFILE '/tmp/kpoly.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
