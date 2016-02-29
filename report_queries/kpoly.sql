0bc202e4eedbe8b77b071126e16a9f2f56f04db7
Dayo Osikoya <alwaysdeone@gmail.com>
2016-02-29 19:45:54 +0100

    Report queries directory.

---
+++ report_queries/kpoly.sql
@@ -0,0 +1,3 @@
+/* Return first_name, last_name and username for users in a given group */
+
+select a.first_name, a.last_name, a.username from auth_user as a join accounts_subscriber as b where a.id=b.user_id and b.group_id=2;
